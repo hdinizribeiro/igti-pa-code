ddf3ef3b55bc6b78d6b3ee03dff62a86c660ca12
hdiniz <hdiniz@ciandt.com>
2021-02-21 11:13:29 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-any
+Código criado para o projeto aplicado do IGTI.
+Inicialmente esse código só contem alguns testes de performance que serão aplicados após a adoção da nova arquitetura das integrações
---
+++ gatling/src/test/scala/calendarEvents/GetEventsPaginationTest.scala
@@ -0,0 +1,39 @@
+package calendarEvents
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+import java.util.Calendar
+import java.text.SimpleDateFormat
+
+class GetEventsPaginationTest extends Simulation {
+
+ val baseHttpProtocol =
+   http.baseUrl("http://freeroom-manager-dev.azurewebsites.net/api")
+
+  val clientId = "c0eff762-9ab1-4da5-8b93-b0f2029fc119"      
+  val locationId = "b6a89e45-6778-4a81-a35b-6bd09a1514de"
+
+  val bearerToken = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkE1Mjg3NzIyMzdFNTI0RTYxMTY5RUM2N0IyQzdFMTZERkY3RTEzQ0MiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJwU2gzSWpmbEpPWVJhZXhuc3NmaGJmOS1FOHcifQ.eyJuYmYiOjE2MTM3NzgwNzMsImV4cCI6MTYxMzc4MTY3MywiaXNzIjoiaHR0cHM6Ly9zdGFnZS1hY2NvdW50cy5mcmVlcm9vbS5pbyIsImF1ZCI6WyJodHRwczovL3N0YWdlLWFjY291bnRzLmZyZWVyb29tLmlvL3Jlc291cmNlcyIsImZyZWVyb29tX2FwaV9yZXNvdXJjZSJdLCJjbGllbnRfaWQiOiJzd2FnZ2VyX2RldiIsImNsaWVudF9mcmVlcm9vbV9pZCI6WyJkNjk1NTIwMS04YjFkLWU4MTEtODBjMi0wMDBkM2FiMTRiMWEiLCJiOGUzNDZhYS04MDlhLTQ4YTktYmIwYy0xMGYxNDk5NGVhZjUiLCJjMGVmZjc2Mi05YWIxLTRkYTUtOGI5My1iMGYyMDI5ZmMxMTkiLCJiZDU3Y2UxZS0yMWQyLTQxZmItOTVhYi1mZDA5YTM3YmJmZmYiLCI2NWU5ZGNiZC0wMzBmLTRmZWMtYjUwYS05MGNlNzkyNTQ0NjIiLCJiMjEzMTg3My03Y2E2LTQzODgtODM5Yy0wZWI0MDRhNWIxOGUiXSwiY2xpZW50X2hhc19hZ2VuZGEiOiJkNjk1NTIwMS04YjFkLWU4MTEtODBjMi0wMDBkM2FiMTRiMWEiLCJjbGllbnRfYmV3ZWxjb21lX2lkIjoiYzc1OGQ5MDEtMTAwOC00NGM3LThjMDktMDU2Y2I5ZTI0MGZlIiwiY2xpZW50X2Rlc2s0bWVfaWQiOiJENjk1NTIwMS04QjFELUU4MTEtODBDMi0wMDBEM0FCMTRCMUEiLCJzdWIiOiIzYTkwMGQxMy00MzJhLTRhYmMtODk1MC00MmI2MGIwYzNjYjUiLCJhdXRoX3RpbWUiOjE2MTM3MzEwNDAsImlkcCI6Imdvb2dsZSIsIm5hbWUiOiJIZW5yaXF1ZSBSaWJlaXJvIiwiZW1haWwiOlsiaGRpbml6QGNpYW5kdC5jb20iLCJoZGluaXpAY2lhbmR0LmNvbSJdLCJtYXRyaWN1bGEiOiJGQUtFLVAwMDMyMyIsInJvbGUiOlsiQWNjb3VudHNBZG1pblJvbGUiLCJkNG1fZ2xvYmFsX2FkbWluIiwiZnJfZ2xvYmFsX2FkbWluIiwiZDRtX3NlcnZpY2VfZGVzayIsImJ3X2dsb2JhbF9hZG1pbiIsImZyX3NlcnZpY2VfZGVzayIsImQ0bV9ncnVwb19ncl9hcGkiXSwic2NvcGUiOlsiZnJlZXJvb21fYXBpX3JlYWRfc2NvcGUiLCJmcmVlcm9vbV9hcGlfd3JpdGVfc2NvcGUiXSwiYW1yIjpbImV4dGVybmFsIl19.L7CYsknT_A1onVQ1JoAWytC2VJwzy-fWNY76DiRqTQXmqaxEpXa33m9fn7H9RIBQK3o6n11NwzLUwFh-qVEQsLRuMY5mxj0uwQbouk_hg3ND2OuhWk-2LrCBehJbny-x1SJKR2FbYskwzmZyNBiV5pYSk4TwYEJtbM186vL6_SmxVJTQhHNm9dp11hQPTk1nJ8M3_inHtOevMdC8AFXS62i3sQMn_73p-0Eu5wc-IiqMB4gX1ClfCeLPmXLi2ELA5KbbsA4A-Y0LgP50huD80CIIbtAV-Adw3IYbKLhs-vzXS4nzQvmBjeHj3ihFNtdEacu-i6wIOAMsvKoP7u3V4Q"
+  val start = Calendar.getInstance()
+  val end = Calendar.getInstance()
+
+  end.add(Calendar.HOUR, 8)
+
+  val startISOString = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(start.getTime())
+  val endISOString = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(end.getTime())
+    
+  val scn = scenario("get-events-pagination-scenario")
+   .exec(
+     http("Free busy")
+       .get(s"api/public/clients/$clientId/events?&offset=0&limit=9&locationId=$locationId&startDate=$startISOString&endDate=$endISOString&numberOfParticipants=1")
+       .header("Authorization", bearerToken)       
+   )
+ 
+ setUp(
+   scn.inject(     
+     constantUsersPerSec(450) during 2.minutes
+   ) 
+ ).protocols(baseHttpProtocol)  
+}
